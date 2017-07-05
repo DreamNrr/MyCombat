19090828ae92d46c1576a43ffaa1d01af100c34b
DreamNrr <wzh74371233@163.com>
2017-07-05 21:28:04 +0800

    BaseActivity

---
+++ app/src/main/java/com/example/wzh/mycombat/base/BaseActivity.java
@@ -0,0 +1,36 @@
+package com.example.wzh.mycombat.base;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by WZH on 2017/7/5.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutId());
+        ButterKnife.inject(this);
+
+        initView();
+
+        initData();
+
+        initListener();
+
+    }
+
+    public void initView() {
+
+    }
+
+
+    public abstract int getLayoutId();
+    public abstract void initData();
+    public abstract void initListener();
+
+}
--- app/src/main/java/com/example/wzh/mycombat/controller/activity/MainActivity.java
+++ app/src/main/java/com/example/wzh/mycombat/controller/activity/MainActivity.java
@@ -1,19 +1,8 @@
 package com.example.wzh.mycombat.controller.activity;
 
-import android.content.Intent;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-import com.example.wzh.mycombat.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-    }
 
 }
