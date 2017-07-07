d3b8cc7d517f137b3714e0a235354ed9f9c2d97f
DreamNrr <wzh74371233@163.com>
2017-07-07 08:26:23 +0800

    gridView显示假数据

---
+++ app/src/main/java/com/example/wzh/mycombat/utils/BaseUrl.java
@@ -0,0 +1,9 @@
+package com.example.wzh.mycombat.utils;
+
+/**
+ * Created by WZH on 2017/7/6.
+ */
+
+public class BaseUrl {
+    public final static String BASE_URL = "http://mobile.iliangcang.com/";
+}
