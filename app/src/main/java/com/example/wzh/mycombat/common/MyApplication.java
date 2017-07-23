5141c8aed3efae20582bc7b13dd25ca3cfbf6d5e
DreamNrr <wzh74371233@163.com>
2017-07-23 14:10:13 +0800

    极光推送

--- app/src/main/java/com/example/wzh/mycombat/common/MyApplication.java
+++ app/src/main/java/com/example/wzh/mycombat/common/MyApplication.java
@@ -8,6 +8,7 @@ import org.xutils.x;
 
 import java.util.concurrent.TimeUnit;
 
+import cn.jpush.android.api.JPushInterface;
 import okhttp3.OkHttpClient;
 
 /**
@@ -30,5 +31,7 @@ public class MyApplication extends Application{
                 .build();
 
         OkHttpUtils.initClient(okHttpClient);
+        JPushInterface.setDebugMode(true); 	// 设置开启日志,发布时请关闭日志
+        JPushInterface.init(this);     		// 初始化 JPush
     }
 }
